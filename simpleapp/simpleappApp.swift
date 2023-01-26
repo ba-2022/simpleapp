9e4a6a48d6f90672fdf373afab30d43d199c5017
ba-2022 <alemube@ssfs.org>
2023-01-26 11:50:20 -0500

    Initial Commit

---
+++ simpleapp/simpleappApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleappApp.swift
+//  simpleapp
+//
+//  Created by Betel on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct simpleappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
